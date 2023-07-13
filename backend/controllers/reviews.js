46cf6c0b19dcaf6f94f50a5bb6ed23e180d12878
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2023-07-13 15:37:17 +0100

    API endpoint to GET all reviews provided by a student user seperated from the API endpoint to GET all reviews provided about a staff user.

--- backend/controllers/reviews.js
+++ backend/controllers/reviews.js
@@ -1,6 +1,7 @@
 import { db } from "../dbConnect.js";
 
-export const getAllUserReviews = (req, res) => {
+//GET ALL STAFF USER'S REVIEWS
+export const getAllStaffUserReviews = (req, res) => {
     const { userId } = req.params;
     
     const q = "SELECT reviews.clarity, reviews.engagement, reviews.communication, reviews.comment, reviews.courseCode, reviews.date, staffs.name as staffName, staffs.department as staffDept, staffs.image as staffImg, students.name  as studentName FROM reviews JOIN staffs ON reviews.staffId = staffs.staffId JOIN students ON reviews.studentId = students.studentId WHERE reviews.staffId = ?"
@@ -18,7 +19,32 @@ export const getAllUserReviews = (req, res) => {
     })
 }
 
+//GET ALL STUDENT USER'S REVIEWS
+export const getAllStudentUserReviews = (req, res) => {
+    const { userId } = req.params;
+    const { studentId } = req.user;
 
+    if(studentId == userId){
+
+        const q = "SELECT reviews.clarity, reviews.engagement, reviews.communication, reviews.comment, reviews.courseCode, reviews.date, staffs.name as staffName, staffs.department as staffDept, staffs.image as staffImg, students.name  as studentName FROM reviews JOIN staffs ON reviews.staffId = staffs.staffId JOIN students ON reviews.studentId = students.studentId WHERE reviews.studentId = ?"
+    
+        db.query(q, [userId], (err, data) => {
+            if(err){
+                res.status(500).json('Server error')
+                console.log(err)
+            }
+            if(data){
+                res.status(200).json(data)
+            }else{
+                res.status(404).json('No review found')
+            }
+        })
+    }else {
+        res.status(404).json('You are not authorized')
+    }
+}
+
+//GET A SINGLE REVIEW
 export const getSingleReview = (req, res) => {
 
     const { id } = req.params;
@@ -38,7 +64,7 @@ export const getSingleReview = (req, res) => {
     })
 }
 
-
+//POST A REVIEW
 export const postReview = (req, res) => {
 
     const { studentId } = req.user;
@@ -61,12 +87,12 @@ export const postReview = (req, res) => {
     })
 }
 
-
+//UPDATE A REVIEW
 export const updateReview = (req, res) => {
 
 }
 
-
+//DELETE A REVIEW
 export const deleteReview = (req, res) => {
 
 }
--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { getAllUserReviews, getSingleReview, postReview, updateReview, deleteReview } from '../controllers/reviews.js';
+import { getSingleReview, postReview, updateReview, deleteReview, getAllStudentUserReviews, getAllStaffUserReviews } from '../controllers/reviews.js';
 import { verifyTokenAndAuthorization, verifyTokenAndAuthorizeStudent } from '../middleware/verifyToken.js';
 
 
 const router = express.Router();
 
 router.post('/', verifyTokenAndAuthorizeStudent, postReview)
-router.get('/find/:userId', verifyTokenAndAuthorization, getAllUserReviews)
+router.get('/find/staffs/:userId', verifyTokenAndAuthorization, getAllStaffUserReviews)
+router.get('/find/students/:userId', verifyTokenAndAuthorizeStudent, getAllStudentUserReviews)
 router.get('/:id', verifyTokenAndAuthorization, getSingleReview)
 router.put('/:id', verifyTokenAndAuthorizeStudent, updateReview)
 router.delete('/:id', verifyTokenAndAuthorizeStudent, deleteReview)
