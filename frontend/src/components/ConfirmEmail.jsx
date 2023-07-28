62257e20997f81f85127495c7819fed10df76320
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2023-07-28 01:49:54 +0100

    Confirm email page design done.

--- frontend/src/components/ConfirmEmail.jsx
+++ frontend/src/components/ConfirmEmail.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const ConfirmEmail = () => {
   return (
-    <main className='confirm-email'>ConfirmEmail</main>
+    <main className='confirm-email'>
+      <div className="content">
+        <h2>Email Verification</h2>
+        <div className="confirm">
+          <h3>LectureRate<span>+</span></h3>
+          <h4>First, let's verify your email</h4>
+          <p>Check <span>johndoe@study.beds.ac.uk</span> to verify your account and get started</p>
+          <div className="btns">
+            <button>Open Email</button>
+            <button>Resend email</button>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
