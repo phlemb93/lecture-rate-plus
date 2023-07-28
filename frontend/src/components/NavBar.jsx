5f151527396fe18ff816da299837bb64714f7388
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2023-07-28 03:49:12 +0100

    Navbar revamped

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import LockResetIcon from '@mui/icons-material/LockReset';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const NavBar = () => {
   return (
@@ -10,9 +13,36 @@ const NavBar = () => {
             </Link>
         </div>
         <div className="menu">
-            <Link to='/about'><a>About</a></Link>
-            <Link to='/login'><a>Login</a></Link>
-            <Link to='/register' className='signup'><a>Sign Up</a></Link>
+            <div className="not-active">
+                <Link to='/about'><a>About</a></Link>
+                <Link to='/register' className='signup'><a>Sign Up</a></Link>
+                <Link to='/login'><a>Login</a></Link>
+            </div>
+            <div className="active">
+                <Link to='#'>Ratings</Link>
+                <div className="profile">
+                    <div className="name">
+                        <p>John Doe</p>
+                        <small>Student</small>
+                    </div>
+                    <ArrowDropDownIcon />
+                    <div className="drop-down">
+                        <Link to='#'>
+                            <div>
+                                <p>Reset password</p>
+                                <LockResetIcon style={{fontSize:'18px'}} />
+                            </div>
+                        </Link>
+                        <Link to='/login'>
+                            <div>
+                                <p>Log out</p>
+                                <LogoutIcon style={{fontSize:'18px'}} />
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     </nav>
   )
