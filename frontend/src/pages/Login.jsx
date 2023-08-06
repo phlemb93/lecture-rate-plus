9851af6bf219d3265d31d7a87299d643cbe6a82e
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2023-08-06 09:00:28 +0100

    Login page error handling done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,25 +1,88 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [verifyError, setVerifyError] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log({email, password})
-       
+    const navigate = useNavigate();
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        setError('');
+        setVerifyError(false);
     }
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+        setError('');
+        setVerifyError(false);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        const values = { email, password };
+
+        if(!email || !password) {
+            return setError('All fields must be filled')
+        }
+    
+            try {
+                const res = await axios.post('http://localhost:8000/api/auth/login', values);
+
+                if(res.status === 200) {
+
+                    //SETTING EMAIL TO LOCAL STORAGE
+                    localStorage.setItem('userEmail', JSON.stringify(res.data));
+
+                    //RETURNING ALL DATA BACK TO DEFAULT
+                    setEmail('');
+                    setPassword('');
+                    setError('');
+
+                    //OPEN EMAIL CONFIRMATION PAGE
+                    console.log(res.data)
+                    // navigate();
+                }
+                
+            } catch (error) {
+                console.log(error)
+                if (error.response.status === 404) {
+
+                    console.log('404 Error')
+                    
+                    if(error.response.data === "Kindly verify your email") {
+                        setVerifyError(true)
+                        setError('Kindly verify your email')
+                    } else {
+                        return setError('Invalid credentials')
+                    }
+                }
+
+                if (error.response.status === 500) {
+                    return setError('There is an error')
+                }
+            }
+    }
+
+
   return (
     <main className='login'>
         <div className='content'>
             <h2>Log In</h2>
+            <div className="error" style={{display: error ? 'flex' : 'none'}}>
+                <p>{ error }</p>
+            </div>
             <form onSubmit={handleSubmit}>
                 <div className="email">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" 
                     placeholder='name@study.beds.ac.uk'
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={handleEmailChange}
                     value={email}
                     /> 
                 </div>
@@ -28,13 +91,16 @@ const Login = () => {
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password"  
                     placeholder='Enter Password' 
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={handlePasswordChange}
                     value={password}
                     />
                     <a href="#">Forgotten Password?</a>
                 </div>
             
-                <button type="submit">Sign In</button>
+                { verifyError ? 
+                <button className='verify-error'><Link to='https://outlook.office.com/mail/inbox/'>Open Email</Link></button>
+                : 
+                <button type="submit">Sign In</button> }
             </form>
         </div>
     </main>
