3217edda7a32a92dfa488a5ae9f534da0f7822d9
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2023-08-10 09:01:28 +0100

    Staff Profile Page logics done

--- frontend/src/pages/StaffProfile.jsx
+++ frontend/src/pages/StaffProfile.jsx
@@ -1,30 +1,48 @@
 import React, { useEffect, useState } from 'react'
 import Chart from 'react-google-charts';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import RateReviewIcon from '@mui/icons-material/RateReview';
 import axios from 'axios';
 
 
 const token = JSON.parse(localStorage.getItem('user')).token;
 
-export const data = [
-    ["Clarity", "Rating"],
-    ["Work", 11],
-    ["Eat", 2],
-    ["Commute", 2],
-    ["Watch TV", 2],
-    ["Sleep", 7],
-  ];
-  
 
 const StaffProfile = () => {
 
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [reviews, setReviews] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [stats, setStats] = useState({});
+  const [staff, setStaff] = useState({staffName: 'John Doe'});
 
+  const firstNameInit = staff.staffName.split(" ")[0].slice(0, 2).toUpperCase();
+
+
+  //CLARITY RATINGS
+  const [numOf1Clarity, setNumOf1Clarity] = useState(0)
+  const [numOf2Clarity, setNumOf2Clarity] = useState(0)
+  const [numOf3Clarity, setNumOf3Clarity] = useState(0)
+  const [numOf4Clarity, setNumOf4Clarity] = useState(0)
+  const [numOf5Clarity, setNumOf5Clarity] = useState(0)
+
+  //ENGAGEMENT RATINGS
+  const [numOf1Engage, setNumOf1Engage] = useState(0)
+  const [numOf2Engage, setNumOf2Engage] = useState(0)
+  const [numOf3Engage, setNumOf3Engage] = useState(0)
+  const [numOf4Engage, setNumOf4Engage] = useState(0)
+  const [numOf5Engage, setNumOf5Engage] = useState(0)
+
+  //COMMUNICATION RATINGS
+  const [numOf1Comm, setNumOf1Comm] = useState(0)
+  const [numOf2Comm, setNumOf2Comm] = useState(0)
+  const [numOf3Comm, setNumOf3Comm] = useState(0)
+  const [numOf4Comm, setNumOf4Comm] = useState(0)
+  const [numOf5Comm, setNumOf5Comm] = useState(0)
+
+
+  //FETCH ALL THE REVIEWS OF THE STAFF MEMBER
     useEffect(() => {
         const getReviews = async () => {
             try {
@@ -36,8 +54,9 @@ const StaffProfile = () => {
                 });
 
                 if(res.status === 200) {
-                    setReviews(res.data)
-                    setIsLoading(false)
+                    setReviews(res.data);
+                    setIsLoading(false);
+                    setStaff(res.data[0]);
                 }
 
             } catch (error) {
@@ -47,52 +66,107 @@ const StaffProfile = () => {
        getReviews();
     }, [])
 
-    // useEffect(() => {
-    //     const getStats = async () => {
-    //         try {
-    //             const res = await axios.get('http://localhost:8000/api/find/staffs/stats/' + id, {
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                     'Authorization': `Bearer ${token}`
-    //                 }
-    //             });
 
-    //             if(res.status === 200) {
-    //                 setStats(res.data)
-    //                 setIsLoading(false)
-    //             }
+//FETCH ALL THE REVIEW STATS FOR THE STAFF MEMBER
+    useEffect(() => {
+        const getStats = async () => {
+            try {
+                const res = await axios.get('http://localhost:8000/api/reviews/find/staffs/stats/' + id, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
 
-    //         } catch (error) {
-    //             console.log(error)
-    //         }
-    //     }
-    //    getStats();
-    // }, [])
+                if(res.status === 200) {
+                    setIsLoading(false)
 
-    console.log(reviews)
-    // console.log(stats)
+                      //CLARITY RATINGS
+                    if(res.data.clarityRating[0][0]) setNumOf5Clarity(res.data.clarityRating[0][0].clarityCount);
+                    if(res.data.clarityRating[0][1]) setNumOf4Clarity(res.data.clarityRating[0][1].clarityCount);
+                    if(res.data.clarityRating[0][2]) setNumOf3Clarity(res.data.clarityRating[0][2].clarityCount);
+                    if(res.data.clarityRating[0][3]) setNumOf2Clarity(res.data.clarityRating[0][3].clarityCount);
+                    if(res.data.clarityRating[0][4]) setNumOf1Clarity(res.data.clarityRating[0][4].clarityCount);
+                  
+                      //ENGAGEMENT RATINGS
+                    if(res.data.engagementRating[0][0]) setNumOf5Engage(res.data.engagementRating[0][0].engageCount);
+                    if(res.data.engagementRating[0][1]) setNumOf4Engage(res.data.engagementRating[0][1].engageCount);
+                    if(res.data.engagementRating[0][2]) setNumOf3Engage(res.data.engagementRating[0][2].engageCount);
+                    if(res.data.engagementRating[0][3]) setNumOf2Engage(res.data.engagementRating[0][3].engageCount);
+                    if(res.data.engagementRating[0][4]) setNumOf1Engage(res.data.engagementRating[0][4].engageCount);
+                  
+                      //COMMUNICATION RATINGS
+                    if(res.data.communicationRating[0][0]) setNumOf5Comm(res.data.communicationRating[0][0].commCount);
+                    if(res.data.communicationRating[0][1]) setNumOf4Comm(res.data.communicationRating[0][1].commCount);
+                    if(res.data.communicationRating[0][2]) setNumOf3Comm(res.data.communicationRating[0][2].commCount);
+                    if(res.data.communicationRating[0][3]) setNumOf2Comm(res.data.communicationRating[0][3].commCount);
+                    if(res.data.communicationRating[0][4]) setNumOf1Comm(res.data.communicationRating[0][4].commCount);
+
+                }
+
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+       getStats();
+    }, [])
+
+
+    //CLARITY CHART DATA
+    const clarityData = [
+      ["Clarity", "Rating"],
+      ["1 Stars", numOf1Clarity],
+      ["2 Stars", numOf2Clarity],
+      ["3 Stars", numOf3Clarity],
+      ["4 Stars", numOf4Clarity],
+      ["5 Stars", numOf5Clarity],
+    ];
+
+     //ENGAGEMENT CHART DATA
+    const engagementData = [
+      ["Engagement", "Rating"],
+      ["1 Stars", numOf1Engage],
+      ["2 Stars", numOf2Engage],
+      ["3 Stars", numOf3Engage],
+      ["4 Stars", numOf4Engage],
+      ["5 Stars", numOf5Engage],
+    ];
+
+     //COMMUNICATION CHART DATA
+    const commData = [
+      ["Communication", "Rating"],
+      ["1 Stars", numOf1Comm],
+      ["2 Stars", numOf2Comm],
+      ["3 Stars", numOf3Comm],
+      ["4 Stars", numOf4Comm],
+      ["5 Stars", numOf5Comm],
+    ];
+
+
+
 
   return (
     <div className='staff-profile'>
         <div className="profile">
           <div className="image">
-            <h1>PW</h1>
+            <h1>{ firstNameInit }</h1>
           </div>
-          <h2>Paul Winter</h2>
-          <small>Mathematics</small>
-          <div className="btn">
+          <h2>{ staff.staffName }</h2>
+          <small>{ staff.staffDept }</small>
+          <div className="btn" onClick={() => navigate('/review')}>
             <RateReviewIcon style={{fontSize: 14}}/>
             <p>Add a Review</p>
           </div>
         </div>
 
-        <p className='ratings-count'>Based on 5 ratings</p>
+        <p className='ratings-count'>Based on <span>{ reviews ? reviews.length : 0 }</span> ratings</p>
         <div className="rating-container">
           <div className="clarity">
             <Chart
                 chartType="PieChart"
-                data={data}
-                options={{title: 'Clarity'}}
+                data={clarityData}
+                options={{ title: 'Clarity', is3D: true }}
                 width={"250px"}
                 height={"300px"}
                 // style={{fontSize: 20, border: '1px solid red'}}
@@ -101,8 +175,8 @@ const StaffProfile = () => {
           <div className="engagement">
             <Chart
                 chartType="PieChart"
-                data={data}
-                options={{title: 'Engagement'}}
+                data={engagementData}
+                options={{ title: 'Engagement', is3D: true }}
                 width={"250px"}
                 height={"300px"}
             />
@@ -110,8 +184,8 @@ const StaffProfile = () => {
           <div className="communication">
             <Chart
                 chartType="PieChart"
-                data={data}
-                options={{title: 'Communication'}}
+                data={commData}
+                options={{ title: 'Communication', is3D: true }}
                 width={"250px"}
                 height={"300px"}
             />
@@ -124,3 +198,31 @@ const StaffProfile = () => {
 }
 
 export default StaffProfile
+
+
+  //CLARITY RATINGS
+    //  if(res.data.clarityRating[0][0].clarityCount === undefined){
+    //   setNumOf5Clarity(0)
+    // }else {
+    //   setNumOf5Clarity(res.data.clarityRating[0][0].clarityCount);
+    // }
+    // if(res.data.clarityRating[0][1].clarityCount === undefined) {
+    //   setNumOf4Clarity(0)
+    // }else {
+    //   setNumOf4Clarity(res.data.clarityRating[0][1].clarityCount);
+    // }
+    // if(res.data.clarityRating[0][2].clarityCount === undefined){
+    //   setNumOf3Clarity(0)
+    // } else {
+    //   setNumOf3Clarity(res.data.clarityRating[0][2].clarityCount);
+    // }
+    // if(res.data.clarityRating[0][3] === undefined){
+    //   setNumOf2Clarity(0)
+    // } else {
+    //   setNumOf2Clarity(res.data.clarityRating[0][3].clarityCount);
+    // }
+    // if(res.data.clarityRating[0][4] === undefined){
+    //   setNumOf1Clarity(0)
+    // }else {
+    //   setNumOf1Clarity(res.data.clarityRating[0][4].clarityCount);
+    //
