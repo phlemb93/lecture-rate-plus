abc48fe5150406f7545faf2739e29fd270af4fca
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2023-09-10 22:55:35 +0100

    fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { IsOpenContextProvider } from '../src/utilities/IsOpenContext';
-import { UserContextProvider } from './utilities/UserContext';
+import { IsOpenContextProvider } from './utilities/IsOpenContext.js';
+import { UserContextProvider } from './utilities/UserContext.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
