15cd0e531c1981732448e695904fa9954af47be7
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2023-09-10 22:48:12 +0100

    Deploy error fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { IsOpenContextProvider } from './utilities/IsOpenContext';
+import { IsOpenContextProvider } from '../src/utilities/IsOpenContext';
 import { UserContextProvider } from './utilities/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
